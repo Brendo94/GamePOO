d4822950d89cb29693d442ac22967b8b34a1c1e8
Jefferson da Silva Barbosa <jefferson.sb.es@gmail.com>
2015-05-01 10:13:26 -0300

    Tela_Inicial e Historia

---
+++ JogoFisica3D/Assets/script/movimentacao.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimentacao : MonoBehaviour {
+
+	public float velocidade = 0.001f;
+	public float aceleracao = 0.001f;
+	public float aceleracaoy = -0.001f;
+	public float velocidadey = -0.001f;
+	public float resistencia = -0.001f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocidadey = aceleracaoy + velocidadey;
+		aceleracaoy = aceleracaoy - resistencia;
+		gameObject.transform.Translate (gameObject.transform.position +  new Vector3(velocidade, velocidadey, 0));
+	}
+
+}
