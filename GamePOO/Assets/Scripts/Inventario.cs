08f7c7ecc1728bb9248ea08e544f50c9ab676651
Brendo Colaço Bomfim Braz <brendocolaco@hotmail.com>
2015-05-01 16:16:32 -0300

    animação do scroll

--- GamePOO/Assets/Scripts/Inventario.cs
+++ GamePOO/Assets/Scripts/Inventario.cs
@@ -8,7 +8,6 @@ public class Inventario : MonoBehaviour {
 	public List<string> myList;
 	public UnityEngine.UI.Button[] botoes;
 
-	// Use this for initialization
 	void Start () {
 		myList = new List<string>();
 		foreach (UnityEngine.UI.Button botao in botoes){
@@ -16,27 +15,9 @@ public class Inventario : MonoBehaviour {
 		}
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
 
 	void OnCollisionEnter (Collision coll){
 		if(coll.gameObject.CompareTag("Scroll")){
-//			myList.Add(coll.gameObject.name);
-//			foreach (string nome in myList){
-//				if(nome == "primeiro"){
-//					botoes[0].interactable = true;
-//				}
-//			}
-//			if(coll.gameObject.name == "primeiro"){
-//				botoes[0].interactable = true;
-//			}
-
-			foreach (UnityEngine.UI.Button botao in botoes){
-
-			}
 
 			botoes[int.Parse(coll.gameObject.name)].interactable = true;
 
@@ -44,13 +25,4 @@ public class Inventario : MonoBehaviour {
 		}
 	}
 
-
-//	public int ListaItens(){
-//		return myList.Count;
-//	}
-//
-//	public void ListaTes(List<string> tes){
-//		myList = tes;
-//		Debug.Log (myList.Count);
-//	}
 }
