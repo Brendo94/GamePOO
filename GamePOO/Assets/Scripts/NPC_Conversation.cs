2bebbc81a379906dcf6458d018f146cae8753942
Brendo Colaço Bomfim Braz <brendocolaco@hotmail.com>
2015-05-03 16:53:59 -0300

    Personagem passando de fase
    Textos da fala do NPC adicionados segundo o game desing, colliders do
    NPC reduzidos.

--- GamePOO/Assets/Scripts/NPC_Conversation.cs
+++ GamePOO/Assets/Scripts/NPC_Conversation.cs
@@ -5,11 +5,11 @@ public class NPC_Conversation : MonoBehaviour {
 
 	public UnityEngine.UI.Text conversa;
 
-	private string dialogo;
+	private static string dialogo;
 
 	// Use this for initialization
 	void Start () {
-		dialogo = "Ola jovem, venha aqui."; 
+		//dialogo = "Ola jovem, eu quero que voce me traga uma classe correta. Dentro dessa caverna existem 5 classes, das quais apenas uma e correta, me traga ela e eu lhe levarei para fora da caverna"; 
 		conversa.text = ""; 
 
 	}
@@ -22,23 +22,27 @@ public class NPC_Conversation : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.CompareTag ("Player")) {
 			if (Pergaminho.classeNoInventario.Equals ("classe01") ) {
-				dialogo = "public class Aranha { \n   private float poder;\n private float custo;\npublic Aranha() {\n poder = 10;\n custo = 15;\n }\n " +
-					"public float atacar(){\n return poder;\n }\n  public void defender(float dano){ \n        custo = custo - dano; \n    } \n}";
+				dialogo = "Correto!!";
+				Application.LoadLevel("segunda_fase");
 			} else if (Pergaminho.classeNoInventario.Equals ("classe02")) {
-				dialogo = "sou classe 02, ola";
+				dialogo = "Hum, nao era essa que eu queria... Esta errada, falta ponto e vírgula no construtor";
 			} else if (Pergaminho.classeNoInventario.Equals ("classe03")) {
-				dialogo = "sou classe 03, ola";
+				dialogo = "Hum, nao era essa que eu queria... Esta errada, o método atacar() não tem retorno, exige um retorno do tipo float";
 			} else if (Pergaminho.classeNoInventario.Equals ("classe04")) {
-				dialogo = "sou classe 04, ola";
+				dialogo = "Hum, nao era essa que eu queria... Esta errada, o método defender() retorna uma atribuição";
 			} else if (Pergaminho.classeNoInventario.Equals ("classe05")) {
-				dialogo = "sou classe 05, ola";
+				dialogo = "Hum, nao era essa que eu queria... Esta errada, o método float atacar() retorna uma String";
+			} else{
+				dialogo = "Ola jovem, eu quero que voce me traga uma classe correta. Dentro dessa caverna existem 5 classes, das quais apenas uma e correta, me traga ela e eu lhe levarei para fora da caverna";
 			}
 				conversa.text = dialogo;
-		}	
+		}
+
 	}
 
 	void OnTriggerExit(Collider other) {
 		if (other.CompareTag ("Player"))
-			conversa.text = "";
+			dialogo = "";
+		conversa.text = dialogo;
 	}
 }
