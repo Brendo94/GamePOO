1597025a259668f77f13ce82953dd2f5b9fbc307
leusonmario <leusonmario@gmail.com>
2015-05-07 18:30:41 -0300

    Adição Mana
    Adição do Mana
    Atualização no código

--- GamePOO/Assets/Scripts/backgroundBehavior.cs
+++ GamePOO/Assets/Scripts/backgroundBehavior.cs
@@ -3,27 +3,38 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class backgroundBehavior : MonoBehaviour {
-	public UnityEngine.UI.Text texto;
-	public int estadoVida;
-	public int maximoVida;
-	private float cachedY;
-	private float minXValue;
-	private float maxXValue;
+	public UnityEngine.UI.Text textoHealth;
+	public UnityEngine.UI.Text textoMana;
 
-	public UnityEngine.UI.Slider slider;
+	public int estadoVida;
+	public int estadoMana;
+	public int maximoVida;
+	public int maximoMana;
+
+	public UnityEngine.UI.Slider sliderHealth;
+	public UnityEngine.UI.Slider sliderMana;
+
 	public Color MaxHealthColor = Color.green;
 	public Color MinHealthColor = Color.red;
+	public Color colorMana = Color.blue;
 	public Image fill;
 
 	// Use this for initialization
 	void Start () {
-		texto.text = "Health: 100";
+		textoHealth.text = "Health: 100";
+		textoMana.text = "Mana: 0";
 		estadoVida = 100;
 		maximoVida = 100;
-		slider.wholeNumbers = true;
-		slider.minValue = 0f;
-		slider.maxValue = maximoVida;
-		slider.value = maximoVida;
+		estadoMana = 0;
+		maximoMana = 100;
+		sliderHealth.wholeNumbers = true;
+		sliderHealth.minValue = 0f;
+		sliderHealth.maxValue = maximoVida;
+		sliderHealth.value = maximoVida;
+		sliderMana.wholeNumbers = true;
+		sliderMana.minValue = 0;
+		sliderMana.maxValue = maximoMana;
+		sliderMana.value = estadoMana;
 	}
 
 	// Update is called once per frame
@@ -33,21 +44,12 @@ public class backgroundBehavior : MonoBehaviour {
 
 	public void atualizarBarra(){
 		if (estouVivo()) {
-			atualizarCorBarra ();
-			atualizarTexto();
+			atualizarHealth (1);
+			UpdateHealthBar (estadoVida);
+			atualizarTextoHealth();
 		}
 	}
-
-	private void atualizarTexto(){
-		texto.text = "Health: " + estadoVida;
-	}
 	
-	private void atualizarCorBarra(){
-		float currentXValue = MapValues(estadoVida,0,maximoVida,minXValue,maxXValue);
-		UpdateHealthBar (estadoVida);
-		estadoVida--;
-	}
-
 	private bool estouVivo(){
 		if (estadoVida >= 1) {
 			return true;
@@ -56,14 +58,44 @@ public class backgroundBehavior : MonoBehaviour {
 		}
 	}
 
+	private void atualizarHealth(int decrescimo){
+		estadoVida -= decrescimo;
+	}
+
+	private void atualizarTextoHealth(){
+		textoHealth.text = "Health: " + estadoVida;
+	}
+
 	public void UpdateHealthBar(int val) {
-		slider.value = val;
+		sliderHealth.value = val;
 		fill.color = Color.Lerp(MinHealthColor, MaxHealthColor, (float)val / maximoVida);
 	}
 	
-	private float MapValues(float x, float inMin, float inMax, float outMin, float outMax){
-		return (x-inMin)*(outMax-outMin)/(inMax-inMin)+outMin;
+	public void aumentarMana(){
+		//neste ponto verifica-se se a adiçao ultrapassa o valor maximo de mana, caso nao esse valor e decrescido, caso contrario iguala ao valor maximo
+		if ((estadoMana + 1) <= maximoMana) {
+			estadoMana++;
+		} else {
+			estadoMana = maximoMana;
+		}
+		atualizarTextoMana ();
+		sliderMana.value = estadoMana;
+
 	}
-	
+
+	public void diminuirMana(){
+		//neste ponto verifica-se se o decrescimo de mana pode ser menor que zero, caso nao executa o if, caso contrario esse valor iguala a zero.
+		if ((estadoMana-1)>=0) {
+			estadoMana--;
+		} else {
+			estadoMana = 0;
+		}
+		atualizarTextoMana ();
+		sliderMana.value = estadoMana;
+	}
+
+	private void atualizarTextoMana(){
+		textoMana.text = "Mana: " + estadoMana;
+	}
 }
  
