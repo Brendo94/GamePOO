33aa0fece7ef42cfdb871d68458dd9cd344833cb
ROGERIO CARVALHO <rogerio_carvalho15@hotmail.com>
2015-05-09 19:38:17 -0300

    primeiros passos arena de batalha
    arena de batalha criada, primeira arquitetura definida etc...

---
+++ GamePOO/Assets/Scripts/GerenciadorSummoner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GerenciadorSummoner : MonoBehaviour {
+	private  GameObject _monstro;
+	public  GameObject[] monstros;
+	public static string nomeMonstro;
+	private bool[] _estaVazio;
+	private Transform[] _filhos;
+
+	// Use this for initialization
+	void Start () {
+		_estaVazio = new bool[4];
+		Invoke ("Instanciar", 1f);
+		Invoke ("Instanciar", 1f);
+		Invoke ("Instanciar", 1f);
+		_estaVazio [0] = true;
+		_estaVazio [1] = true;
+		_estaVazio [2] = true;
+		_filhos = GetComponentsInChildren<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Instanciar() {
+		//Criar chave-valor pra instanciar os monstros
+		// USAR SENDMESSAGE PARA PASSAR PARAMETROS ENTRE CLASSE MONOBEHAVIOUR
+		if(nomeMonstro == "Aranha"){
+			_monstro = GameObject.Instantiate (monstros [0]) as GameObject;
+
+		}
+
+		if (_estaVazio [0]) {
+			_monstro.transform.position =  _filhos[1].transform.position;
+			_estaVazio[0] = false;
+		}else if (_estaVazio [1]) {
+			_monstro.transform.position =  _filhos[2].transform.position;
+			_estaVazio[1] = false;
+		}else if (_estaVazio [2]) {
+			_monstro.transform.position =  _filhos[3].transform.position;
+			_estaVazio[2] = false;
+		}
+	}
+}
---
+++ GamePOO/Assets/Scripts/LoadMonstroArena.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMonstroArena : MonoBehaviour {
+	private GameObject _monstro;
+	public GameObject[] monstros;
+
+
+	// Use this for initialization
+	void Start () {
+		_monstro = GameObject.Instantiate (monstros [1]) as GameObject;
+		_monstro.transform.position = transform.position;
+		GerenciadorSummoner.nomeMonstro = "Aranha";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
